68f2cf2a2022a447baee995ff99f931599dc373d
lucasrluz <lucasr.luzbr@gmail.com>
2021-05-28 20:04:07 -0300

    suporte para receber json no body

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
-import express from 'express'
+import express, { json } from 'express'
 import { userRoute } from './routes/userRoute'
 
 const app = express()
+
+app.use(json())
 app.use(userRoute)
 app.listen(3333)
