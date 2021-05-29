2c74f6054834b99d32f1ba0c3a59e7e788f3d4af
lucasrluz <lucasr.luzbr@gmail.com>
2021-05-29 11:55:44 -0300

    configuração de acesso em rotas

--- src/routes/userRoute.ts
+++ src/routes/userRoute.ts
@@ -1,6 +1,7 @@
 import express from 'express'
 import { connection } from '../connection/connection'
 import { User } from '../entity/User'
+import { auth } from '../middlewares/auth'
 import { UserService } from '../service/userService'
 
 export const userRoute = express.Router()
@@ -12,14 +13,16 @@ userRoute.post('/session', async (req, res) => {
     res.json(await userService.login(req.body))
 })
 
-userRoute.get('/user/:id_user', async (req, res) => {
-    
-    res.json(await userService.viewUser(req.params.id_user))
-})
-
 userRoute.post('/user', async(req, res) => {
 
     res.json(await userService.saveUser(req.body))
+})
+
+userRoute.use(auth)
+
+userRoute.get('/user/:id_user', async (req, res) => {
+    
+    res.json(await userService.viewUser(req.params.id_user))
 })
 
 userRoute.put('/user/:id_user', async (req, res) => {
