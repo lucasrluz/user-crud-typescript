aeab23e85e7d837d8ed7690ca0ea30b0cae49ebe
lucasrluz <lucasr.luzbr@gmail.com>
2021-06-01 10:03:26 -0300

    refatoração completa da classe de acordo com a classe dataAccess

--- src/service/userService.ts
+++ src/service/userService.ts
@@ -2,7 +2,11 @@ import { connection } from "../connection/connection";
 import { User } from "../entity/User";
 import * as bcrypt from 'bcrypt'
 import * as jwt from 'jsonwebtoken'
+import { validator } from "../classValidator/userValidator";
 import { NextFunction, Request, Response } from "express";
+import { UserDataAccess } from "../dataAccess/userDataAccess";
+
+const userDataAccess = new UserDataAccess()
 export class UserService {
 
     async login(request: Request, response: Response, next: NextFunction) {
@@ -43,81 +47,90 @@ export class UserService {
     }
 
     async viewUser(request: Request, response: Response, next: NextFunction) {
-
-        const result = await (await connection)
-            .createQueryBuilder()
-            .select('user')
-            .from(User, 'user')
-            .where('id_user = :id_user', {id_user: request.params.id_user})
-            .getOne()
-    
-        if (result == null) return response.status(404).json({message: "Este usuário não existe!"})
-
-        return response.status(201).json(result)
-    }
-
-    async saveUser(request: Request, response: Response, next: NextFunction) {
-
         try {
 
-            const user = request.body
+            const id_user = request.params.id_user
 
-            const passwordHash = await bcrypt.hash(user.password, 8)
+            const result = await userDataAccess.viewUser(id_user)
+            
+            if (result == null) return response.status(404).json({message: "Este usuário não existe!"})
 
-            const result = await (await connection)
-                .createQueryBuilder()
-                .insert()
-                .into(User)
-                .values([{ name: user.name, email: user.email, password: passwordHash }])
-                .execute()
-        
-            return response.status(201).json(result.identifiers)
+            return response.status(201).json(result)
 
         } catch (error) {
             
-            if (error.code == 'ER_DUP_ENTRY') return response.status(500).json({error: 'Este e-mail já está cadastrado!'}) 
+            return response.status(500).json(error)
+        }   
+    }
 
+    async saveUser(request: Request, response: Response, next: NextFunction) {
+        try {
+
+            const user: User = request.body
+
+            const errors = await validator(user)
+
+            if (errors.length > 0) return response.status(500).json(errors)
+
+            const passwordHash = await bcrypt.hash(user.password, 8) 
+
+            user.password = passwordHash
+
+            const result = await userDataAccess.saveUser(user)
+            
+            if (result.code == 'ER_DUP_ENTRY') return response.status(500).json({error: 'Este e-mail já está cadastrado!'}) 
+
+            return response.status(201).json(result.identifiers)
+            
+        } catch (error) {
+            
             return response.status(500).json(error)
         }
     }
 
     async editUser(request: Request, response: Response, next: NextFunction) {
-
         try {
 
-            const user = request.body
+            const user: User = request.body
+
             const id_user = request.params.id_user
+
+            const errors = await validator(user)
+
+            if (errors.length > 0) return response.status(500).json(errors)
 
             const passwordHash = await bcrypt.hash(user.password, 8)
 
-            const result = await (await connection)
-                .createQueryBuilder()
-                .update(User)
-                .set({name: user.name, email: user.email, password: passwordHash})
-                .where('id_user = :id_user', {id_user: id_user})
-                .execute()
+            user.password = passwordHash
         
+            const result = await userDataAccess.editUser(id_user, user)
+
+            if (result.code == 'ER_DUP_ENTRY') return response.status(500).json({error: 'Este e-mail já está cadastrado!'}) 
+
             return response.status(204).json(result)
 
         } catch (error) {
             
-            if (error.code == 'ER_DUP_ENTRY') return response.status(500).json({error: 'Este e-mail já está cadastrado!'}) 
-
             return response.status(500).json(error)
         }
     }
 
     async deleteUser(request: Request, response: Response, next: NextFunction) {
+        try {
 
-        const id_user = request.params.id_user
+            const id_user = request.params.id_user
 
-        const result = await (await connection)
-            .createQueryBuilder()
-            .delete()
-            .from(User)
-            .where('id_user = :id_user', {id_user: id_user})
-            .execute()
+            const validation = await userDataAccess.viewUser(id_user)
 
-        return response.status(204).json(result)
+            if (validation == null) return response.status(404).json({message: "Este usuário não existe!"})
+
+            const result = await userDataAccess.deleteUser(id_user)
+            
+            return response.status(204).json(result) 
+
+        } catch (error) {
+            
+            return response.status(500).json(error)
+        }     
     }
 }
