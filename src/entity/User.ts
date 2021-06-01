491d5a4c7fb927c100292f894083cdd55ddba1e1
lucasrluz <lucasr.luzbr@gmail.com>
2021-06-01 10:02:33 -0300

    validações de campos e construtor da classe

--- src/entity/User.ts
+++ src/entity/User.ts
@@ -1,17 +1,27 @@
+import { IsEmail, IsNotEmpty } from 'class-validator'
 import { Entity, PrimaryGeneratedColumn, Column} from 'typeorm'
 
 @Entity()
 export class User {
 
+    constructor(name: string, email: string, password: string) {
+        this.name = name,
+        this.email = email,
+        this.password = password
+    }
+
     @PrimaryGeneratedColumn()
     id_user: number
 
     @Column()
+    @IsNotEmpty()
     name: string
 
     @Column({unique: true})
+    @IsEmail()
     email: string
 
     @Column()
+    @IsNotEmpty()
     password: string
 }
